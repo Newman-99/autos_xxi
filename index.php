9107fc97c6c8d173b70d9908ad0375f1076a95c0
newman <newmanrodriguez1999@gmail.com>
2019-08-10 22:09:45 -0400

    Dasai

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include("header.php"); ?>
 
 <h1 style="text-align:center">
-    Sistema De Gestion Automotriz
+    Sistema De Gestion Automotriz Dasai
 </h1>
 
 <?php include("footer.php"); ?>
